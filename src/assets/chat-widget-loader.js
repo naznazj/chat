1c8b27535f997e5f15f010df4dc295be4794393e
Joe Michael <joemichaeltanilon@gmail.com>
2025-01-20 10:07:58 +0800

    chat-widget2

--- src/assets/chat-widget-loader.js
+++ src/assets/chat-widget-loader.js
@@ -1,10 +1,12 @@
 //create a chat widget
 (function() {
   var script = document.createElement('script');
-  script.src = '/dist/frontend/main.js'; // Adjust the path to your Angular app's main.js
+  script.src = 'https://chat-75vz.vercel.app/dist/frontend/browser/main.js'; // Adjust the path to your Angular app's main.js
   script.onload = function() {
-    var chatWidget = document.createElement('app-chat-widget');
-    document.body.appendChild(chatWidget);
+    document.addEventListener('DOMContentLoaded', function() {
+      var chatWidget = document.createElement('app-chat-widget');
+      document.body.appendChild(chatWidget);
+    });
   };
   script.onerror = function(event) {
     console.error('Failed to load chat widget:', event);
