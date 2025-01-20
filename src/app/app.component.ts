9153f51fe78d62a86229c46930d4dfd195f56f13
Joe Michael <joemichaeltanilon@gmail.com>
2025-01-20 14:39:23 +0800

    cw-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatWidgetComponent } from './chat-widget/chat-widget.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ChatWidgetComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
